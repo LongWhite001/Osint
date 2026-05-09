11f1fdfd2dfc3cf9bf021a5b0818b7a516967847
Dragon-White <dbranco200@gmail.com>
2026-05-09 12:26:04 -0300

    Add files via upload

---
+++ 06 - Boitata Google Chrome.bat
@@ -0,0 +1,23 @@
+@echo off
+
+pause
+cls
+
+echo Boitata contem multiplas ferramentas para OSINT
+echo #
+echo e Verificacao de Virus em Arquivos com a opcao
+echo #
+echo Sandbox de Malwere, e muitas outras ferramentas.
+echo #
+echo Ferramentas para Verificacao de Virus recomendadas:
+echo #
+echo ###################
+echo Hybrid analysis
+echo virus total
+echo Meta Defender
+echo ###################
+
+echo Iniciando Boitata no Navegador:
+pause
+start chrome.exe https://boitata.app.br/
+
