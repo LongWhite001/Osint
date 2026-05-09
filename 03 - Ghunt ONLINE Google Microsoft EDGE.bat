41334497db9591c7c2b59bc0551c32be8a368cd0
Dragon-White <dbranco200@gmail.com>
2026-05-09 12:21:22 -0300

    Add files via upload

---
+++ 03 - Ghunt ONLINE Google Microsoft EDGE.bat
@@ -0,0 +1,17 @@
+@echo off
+
+chcp 65001 >nul
+title GHunt Google Hunter: Localizar Usuarios GMAIL Online-Mode.
+:: Função para escrever texto lentamente
+setlocal enabledelayedexpansion
+set "texto=>>>>>>>>>>>>>>>>>>>>CARREGANDO>>>>>>>>>>>>>>>>>>>>"
+for /l %%i in (0,1,50) do (
+set "char=!texto:~%%i,1!"
+<nul set /p="!char!"
+ping -n 1 -w 120 localhost >nul
+)
+echo.
+echo Se nao carregar o site pode ter sido Desativado!
+timeout /t 5 >nul
+cls
+Start msedge.exe https://gmail-osint.activetk.jp/#google_vignette
---
+++ 04 - NAMINT Brave.bat
@@ -0,0 +1,27 @@
+@echo off
+
+Title: NAMINT
+echo Ferramenta de Investigacao de Contas de Usuario NAMINT.
+echo -----
+echo .
+echo .LEIA...
+echo Digite o primeiro nome nome do meio ou apelido e sobrenome e pressione Ir! 
+echo para ver possiveis padroes de pesquisa e links Google Yandex Facebook Twitter
+echo Linkedin e outras redes sociais.
+timeout /t 10
+cls
+
+chcp 65001 >nul
+
+:: Função para escrever texto lentamente
+setlocal enabledelayedexpansion
+set "texto=::::::::::::CARREGANDO::::::::::::"
+for /l %%i in (0,1,36) do (
+set "char=!texto:~%%i,1!"
+<nul set /p="!char!"
+ping -n 1 -w 100 localhost >nul
+)
+echo.
+timeout /t 1 >nul
+cls
+start brave.exe https://seintpl.github.io/NAMINT/
